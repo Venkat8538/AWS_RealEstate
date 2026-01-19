037a1bbe571b621d6e486791e6a8f27b2709e286
Venkat8538 <your-email@example.com>
2026-01-19 15:39:50 -0600

    removed unnecessary files

--- docker/training/mlflow_helper.py
+++
@@ -1,8 +0,0 @@
-import os
-import mlflow
-
-def init_mlflow(run_name: str):
-    tracking_uri = os.environ.get("MLFLOW_TRACKING_URI", "http://mlflow-service:5000")
-    mlflow.set_tracking_uri(tracking_uri)
-    mlflow.set_experiment("house-price-prediction")
-    return mlflow.start_run(run_name=run_name)
